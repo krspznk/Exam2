d90c8282c7d7c4dee170c7a691b1ccfd56e9bb54
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-06-03 17:25:40 +0300

    Change Ex2

--- ConsoleApplication9/ex2.cpp
+++ ConsoleApplication9/ex2.cpp
@@ -7,66 +7,66 @@ using namespace std;
 struct ClassRoom
 {
 public:
-	int No;
-	float D;
-	float W;
+    int No;
+    float D;
+    float W;
 };
 
 
 void InputInformation(vector<ClassRoom>& R, int No)
 {
-	ClassRoom temp;
-	for (int i = 0; i < No; i++)
-	{
-		cout << "Enter information for " << i + 1 << " room:" << endl;
-		cout << "Nomber of room: "; cin >> temp.No;
-		cout << "length: "; cin >> temp.D;
-		cout << "width: "; cin >> temp.W;
-		R.push_back(temp);
-	}
+    ClassRoom temp;
+    for (int i = 0; i < No; i++)
+    {
+        cout << "Enter information for " << i + 1 << " room:" << endl;
+        cout << "Nomber of room: "; cin >> temp.No;
+        cout << "length: "; cin >> temp.D;
+        cout << "width: "; cin >> temp.W;
+        R.push_back(temp);
+    }
 }
 
 
 void OutputInformation(vector<ClassRoom>& R, int No)
 {
-	cout << setw(10) << "Number" << setw(10) << "length" << setw(10) << "width" << endl;
-	for (int i = 0; i < No; i++)
-	{
-		cout << setw(10) << R[i].No << setw(10) << R[i].D << setw(10) << R[i].W << endl;
-	}
+    cout << setw(10) << "Number" << setw(10) << "length" << setw(10) << "width" << endl;
+    for (int i = 0; i < No; i++)
+    {
+        cout << setw(10) << R[i].No << setw(10) << R[i].D << setw(10) << R[i].W << endl;
+    }
 }
 
 int main()
 {
-	int No, K, a=0, b=0;
-	vector<ClassRoom> R;
-	vector<ClassRoom> A;
-	vector<ClassRoom> B;
-	vector <int> Comp;
-	cout << "Enter number of rooms: ";
-	cin >> No;
+    int No, K, a = 0, b = 0;
+    vector<ClassRoom> R;
+    vector<ClassRoom> A;
+    vector<ClassRoom> B;
+    vector <int> Comp;
+    cout << "Enter number of rooms: ";
+    cin >> No;
 
-	InputInformation(R, No);
+    InputInformation(R, No);
 
-	cout << "Enter amoung of computers: ";
-	cin >> K;
+    cout << "Enter amoung of computers: ";
+    cin >> K;
 
-	for (int i = 0; i < No; i++)
-	{
-		if ((R[i].D * R[i].W) >= K * 3) {
-			A.push_back(R[i]);
-			a++;
-		}
-		else {
-			B.push_back(R[i]);
-			b++;
-		}
-	}
+    for (int i = 0; i < No; i++)
+    {
+        if ((R[i].D * R[i].W) >= K * 3) {
+            A.push_back(R[i]);
+            a++;
+        }
+        else {
+            B.push_back(R[i]);
+            b++;
+        }
+    }
 
-	cout << setw(10) << "A vector: " << endl;
-	OutputInformation(A, a);
-	cout << endl;
-	cout << setw(10) << "B vector: " << endl;
-	OutputInformation(B, b);
-	return 0;
+    cout << setw(10) << "A vector: " << endl;
+    OutputInformation(A, a);
+    cout << endl;
+    cout << setw(10) << "B vector: " << endl;
+    OutputInformation(B, b);
+    return 0;
 }
